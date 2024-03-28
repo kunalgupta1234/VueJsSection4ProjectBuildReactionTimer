d69308c0dc2d2f46d15dc662c9d6c142b5511562
KunalGupta <g.kunal9@gmail.com>
2024-03-28 16:57:30 +0530

    35-emitting custom events with data

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
-  <MyBlock v-if="isPlaying" :delay="delay"/>
+  <MyBlock v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResult">Reaction Time : {{ score }} ms</p>
 </template>
 
 <script>
-import MyBlock from './components/MyBlock.vue'
+import MyBlock from "./components/MyBlock.vue";
 export default {
   name: "App",
-  components: {MyBlock},
+  components: { MyBlock },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResult: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
     },
   },
 };
